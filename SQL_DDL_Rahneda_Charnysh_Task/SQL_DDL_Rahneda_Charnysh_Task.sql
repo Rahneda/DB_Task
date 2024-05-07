6b69faa0ea4805095e3094be00d16144014b2b4e
rahnedacharnysh <rahneda91@gmail.com>
2024-05-07 23:59:22 +0300

    done

--- SQL_DDL_Rahneda_Charnysh_Task/SQL_DDL_Rahneda_Charnysh_Task.sql
+++ SQL_DDL_Rahneda_Charnysh_Task/SQL_DDL_Rahneda_Charnysh_Task.sql
@@ -25,14 +25,6 @@ create table Climber (
     address varchar not null
 );
 
-create table Climb (
-    climb_id serial primary key,
-    climber_id int not null ,
-    mountain_id int not null ,
-    start_date date not null ,
-    end_date date not null
-);
-
 create table Mountain(
     mountain_id serial primary key ,
     name varchar not null ,
@@ -47,3 +39,56 @@ create table Mountain(
                      references Ares(area_id)
 );
 
+create table Climb_Type(
+    climb_type_id serial primary key ,
+    climber_id int not null ,
+    mountain_id int not null ,
+
+    constraint fk_climber_id
+                       foreign key (climber_id)
+                       references Climber(climber_id),
+    constraint fk_mountain_id
+                       foreign key (mountain_id)
+                       references Mountain(mountain_id)
+);
+
+create table Climb(
+    climb_id serial primary key,
+    climber_id int not null ,
+    mountain_id int not null ,
+    start_date date not null ,
+    end_date date not null,
+
+    constraint fk_climber_id
+                  foreign key (climber_id)
+                  references Climber(climber_id),
+
+    constraint fk_mountain_id
+                  foreign key (mountain_id)
+                  references Mountain(mountain_id)
+);
+
+create table Climb_Difficulty(
+    climb_id int not null ,
+    difficulty_level_id int not null ,
+
+    constraint fk_climb_id
+                             foreign key (climb_id)
+                             references Climb(climb_id),
+
+    constraint fk_difficulty_level_id
+                             foreign key (difficulty_level_id)
+                             references Difficulty_Level(difficulty_level_id)
+);
+
+create table Climb_Equipment(
+    climb_id int not null ,
+    equipment_id int not null ,
+
+    constraint fk_climb_id
+                            foreign key (climb_id)
+                            references Climb(climb_id),
+    constraint fk_equipment_id
+                            foreign key (equipment_id)
+                            references Equipment(equipment_id)
+);
